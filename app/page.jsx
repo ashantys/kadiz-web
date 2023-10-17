1665290733db7c073017334c3d10819efae1d622
ashantys <ashantys.m@gmail.com>
2023-10-16 18:00:06 -0600

    Ready

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
 'use client';
 import 'bootstrap/dist/js/bootstrap.min.js'
-import Header from "components/Header";
-import Navbar from "components/Navbar";
-import Nosotros from "components/Nosotros";
-import Servicios from "components/Servicios";
-import Contacto from "components/Contacto";
-import Footer from "components/Footer";
+import Header from "../pages/Header";
+import Navbar from "../pages/Navbar";
+import Nosotros from "../pages/Nosotros";
+import Servicios from "../pages/Servicios";
+import Contacto from "../pages/Contacto";
+import Footer from "../pages/Footer";
 
 
 export default function Home() {
